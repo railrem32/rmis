cf757ce5fa91547ccaac4e8da9ebe35225b9617f
e1_d <molokanov.dmitriy@gmail.com>
2016-08-22 16:37:08 +0300

    filter entity

---
+++ src/main/java/ru/dz/rmis/model/Filter.java
@@ -0,0 +1,38 @@
+package ru.dz.rmis.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Filter")
+public class Filter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String className;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
